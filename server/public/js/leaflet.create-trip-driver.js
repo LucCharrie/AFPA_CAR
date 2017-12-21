0361fe2da2792a4c9e8f0bd6139862bb301e850a
jsahaway <jsahaway@gmail.com>
2017-12-21 10:52:42 +0100

    leaflet commit

--- server/public/js/leaflet.create-trip-driver.js
+++ server/public/js/leaflet.create-trip-driver.js
@@ -1,9 +1,9 @@
 // set center coordinates
-var centerlat = 45.12;
-var centerlon = 1.3;
+var centerlat = 43.30;
+var centerlon = 5.36;
 
 // set default zoom level
-var zoomLevel = 11; 
+var zoomLevel = 10;
 
 var map = L.map('map').setView([centerlat, centerlon], zoomLevel);
 
@@ -12,36 +12,75 @@ var addressArrival = new kt.OsmNamesAutocomplete('address_arrival', 'https://geo
 
 var path = new Array(2);
 
-addressDeparture.registerCallback(function(item) {
+addressDeparture.registerCallback(function (item) {
     path[0] = L.latLng(item.lat, item.lon);
 });
 
-addressArrival.registerCallback(function(item) {
+addressArrival.registerCallback(function (item) {
     path[path.length] = L.latLng(item.lat, item.lon);
 });
 
 //var r = new L.Routing.OSRMv1({serviceUrl: 'https://router.project-osrm.org/route/v1/'});
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var control = L.Routing.control({
-    //router: r, // http://10.111.61.7:5000/route/v1
+var router = new L.Routing.osrmv1({
+    serviceUrl: 'http://10.111.61.97:5000/route/v1'
+});
 
-    routeWhileDragging: true
-}).addTo(map);
+var latMerge;
+var lngMerge;
 
-var r = control.getRouter();
+var routeWaypoints = [
+    L.Routing.waypoint(L.latLng(43.30, 5.37)),
+    L.Routing.waypoint(L.latLng(43.30, 5.40))
+];
 
-r.route({
-    waypoints: [
-        L.latLng(45.12755999999999,1.305565999999999),
-        L.latLng(45.159555,1.5339370000000372)
-    ]}, function(err, routes) {
-        console.log(err);
-        console.log('recalc');
-}, map);
+var averageOfRoute = {
+    lat: ( routeWaypoints[0].latLng.lat + routeWaypoints[ routeWaypoints.length - 1 ].latLng.lat ) / 2,
+    lng: ( routeWaypoints[0].latLng.lng + routeWaypoints[ routeWaypoints.length - 1 ].latLng.lng ) / 2
+}
+
+
+router.route(routeWaypoints, (err, routes) => {
+
+    // var routeline = L.Routing.line(routes[0], {
+    //     styles: [{
+    //         color: 'black',
+    //         opacity: 0.7,
+    //         weight: 5
+    //     }]
+    // });
+
+    var routeline = L.Routing.line(routes[0]);
+
+
+    console.log(routeWaypoints.length)
+    console.log(averageOfRoute )
+    
+    map.flyTo([averageOfRoute.lat, averageOfRoute.lng], 14);
+    L.layerGroup([routeline]).addTo(map);
+
+}, null, {});
+
+// var control = L.Routing.control({
+//     //router: r, // http://10.111.61.7:5000/route/v1
+
+//     routeWhileDragging: true
+// }).addTo(map);
+
+// var r = control.getRouter();
+
+// r.route({
+//     waypoints: [
+//         L.latLng(45.12755999999999,1.305565999999999),
+//         L.latLng(45.159555,1.5339370000000372)
+//     ]}, function(err, routes) {
+//         console.log(err);
+//         console.log('recalc');
+// }, map);
 
 //L.Routing.Control().getRouter();
 
