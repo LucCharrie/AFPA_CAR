c93d5a07cab09c405977800a29ec2c85e7d9002a
guillaumeTiertant <tiertant@gmx.fr>
2018-01-22 14:00:12 +0100

    patch autocomplete car

--- server/public/js/pages/addresses-user/list-addresses-user.client.js
+++ server/public/js/pages/addresses-user/list-addresses-user.client.js
@@ -1,4 +1,4 @@
-var listCarsUserController = {};
+var listAddressUserController = {};
 
 (function (self) {
 
@@ -8,28 +8,28 @@ var listCarsUserController = {};
     //
     self.init = function () {
         $.ajax({
-            url: "/api/cars-user/me",
+            url: "/api/address-user/me",
             type: "GET",
             dataType: "json",
             success: function (data) {
-                self.carsUserListSuccess(data);
+                self.AddressUserListSuccess(data);
             }
         });
     }
 
-    self.carsUserListSuccess = function(carsUser) {
-        $.each(carsUser, function (index, carUser) {
-          self.carUserAddCard(carUser.id, carUser);
+    self.AddressUserListSuccess = function(AddressUser) {
+        $.each(carsUser, function (index, addressUser) {
+          self.addressUserAddCard(addressUser.id, addressUser);
         });
     }
 
-    self.carUserAddCard = function(id, carUser) {
-         $(".ui.cards").append(self.carUserBuildCard(id, carUser));
+    self.addressUserAddCard = function(id, addressUser) {
+         $(".ui.cards").append(self.addressUserBuildCard(id, addressUser));
     }
 
-    self.carUserDeleteCard = function(id) {
+    self.addressUserDeleteCard = function(id) {
         $.ajax({
-            url: "/api/cars-user/me/" + id,
+            url: "/api/addresses-user/me/" + id,
             type: 'DELETE',
             dataType: "json",
             success: function (data) {
@@ -39,7 +39,7 @@ var listCarsUserController = {};
         });
     }
 
-    self.carUserBuildCard = function(id, carUser) {
+    self.addressUserBuildCard = function(id, addressUser) {
         var card =
         "<div class='card card-trip-driver' data-id='" + id + "'>" +
             "<div class='content'>" +
@@ -50,25 +50,25 @@ var listCarsUserController = {};
                     "<tbody>" +
                     "<tr>" +
                         "<td> Marque : </td>" +
-                        "<td>" + carUser.car.brand.brand_name + "</td>" +
+                        "<td>" + addressUser.address.brand.brand_name + "</td>" +
                     "</tr>" +
                     "<tr>" +
                         "<td> Modèle : </td>" +
-                        "<td>" + carUser.car.model_name + "</td>" +
+                        "<td>" + addressUser.address.model_name + "</td>" +
                     "</tr>" +
                     "<tr>" +
                         "<td> Couleur :</td>" +
-                        "<td>" + carUser.color + "</td>" +
+                        "<td>" + addressUser.color + "</td>" +
                     "</tr>" +
                     "<tr>" +
                         "<td> Immatriculation :</td>" +
-                        "<td>" + carUser.numimmat + "</td>" +
+                        "<td>" + addressUser.numimmat + "</td>" +
                     "</tr>" +
                 "</table>" +
             "</div>" +
             "<div class='extra content'>" +
                 "<div class='ui basic blue button'> Editer</div>" +
-                "<div class='ui basic red button' onclick='listCarsUserController.carUserDeleteCard(" + id + ")'> Supprimer</div>" +
+                "<div class='ui basic red button' onclick='listaddressUserController.addressUserDeleteCard(" + id + ")'> Supprimer</div>" +
             "</div>" +
         "</div>";
         
@@ -82,4 +82,4 @@ var listCarsUserController = {};
     window.onload = self.init;
     
 
-})(listCarsUserController);
+})(listAddressUserController);
