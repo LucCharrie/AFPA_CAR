84d86832086de98af6219acfe370648a3597ce34
AntiLoxy <rootofgeno@gmail.com>
2018-01-24 18:17:23 +0100

    for gui

--- server/api/addresses-user/services/addresses-user.service.js
+++ server/api/addresses-user/services/addresses-user.service.js
@@ -8,59 +8,55 @@ let AddressesUserDAO  = require('../dao/addresses-user.dao.mysql');
 class AddressesUserService
 {
     static listByUserID(idUser, cb) {
-        return AddressesUserDAO.listByUserID(idUser, (err, AddressUserReturned) => {
-                        if (err) {
+        return AddressesUserDAO.listByUserID(idUser, (err, addressUserReturned) => {
+            if (err) {
                 console.error(err);
             }
 
-            cb(err, AddressUserReturned);
+            cb(err, addressUserReturned);
         });
     }
 
     static delete(id, cb) {
-        return AddressesUserDAO.delete(id, (err, carUserReturned) => {
+        return AddressesUserDAO.delete(id, (err, addressUserReturned) => {
             if (err) {
                 console.error(err);
             }
 
-            cb(err, AddressUserReturned);
+            cb(err, addressUserReturned);
         });
     }    
 
     static deleteByUserID(id, idUser, cb) {
-        return AddressesUserDAO.deleteByUserID(id, idUser, (err, carUserReturned) => {
+        return AddressesUserDAO.deleteByUserID(id, idUser, (err, addressUserReturned) => {
             if (err) {
                 console.error(err);
             }
 
-            cb(err, AddressUserReturned);
+            cb(err, addressUserReturned);
         });
     }
 
-    static create(AddressUser, cb) {
+    static create(addressUser, cb) {
         
-        return AddressesUserDAO.create(AddressUser, (err, carUserReturned) => {
-            console.log(2);
-            if (err) {
-                console.log(3);
-                console.error(err);
-            }
-
-            cb(err, AddressUserReturned);
-        });
-    }
-
-    static createGPS(AddressUser, cb) {
-        return AddressesUserDAO.create(AddressUser, (err, carUserReturned) => {//createGPS dans DAO
+        return AddressesUserDAO.create(addressUser, (err, addressUserReturned) => {
             if (err) {
                 console.error(err);
             }
 
-            cb(err, AddressUserReturned);
+            cb(err, addressUserReturned);
         });
     }
 
+    static createGPS(addressUser, cb) {
+        return AddressesUserDAO.create(addressUser, (err, addressUserReturned) => {//createGPS dans DAO
+            if (err) {
+                console.error(err);
+            }
 
+            cb(err, addressUserReturned);
+        });
+    }
 }
 
 module.exports = AddressesUserService;
